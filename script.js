4d60e39bfb81e288e66e8fde6d90f6b196fb717b
gitxtra <75396266+gitxtra@users.noreply.github.com>
2021-03-08 05:21:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.modal-child').on('show.bs.modal', function () {
+    var modalParent = $(this).attr('data-modal-parent');
+    $(modalParent).css('opacity', 0);
+});
+ 
+$('.modal-child').on('hidden.bs.modal', function () {
+    var modalParent = $(this).attr('data-modal-parent');
+    $(modalParent).css('opacity', 1);
+});
